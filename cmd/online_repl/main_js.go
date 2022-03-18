177a14661c72c5fad15216273097dfdc71f5b36f
Alexander Voykov <foldzip@gmail.com>
2022-03-18 16:23:17 +0300

    Use print sexpr instead of golang primitives

--- cmd/online_repl/main_js.go
+++ cmd/online_repl/main_js.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 
 	"github.com/adzeitor/goscheme/scheme"
+	"github.com/adzeitor/goscheme/sexpr"
 )
 
 func main() {
 	js.Global().Set("schemeEval", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		result := scheme.Eval(args[0].String())
-		output := fmt.Sprint(result)
+		output := sexpr.Print(result)
 		return output
 	}))
 	select {} // Code must not finish
